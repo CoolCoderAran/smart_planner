8c11433d033bf8997853e54a35807067c92bff94
CoolCoderAran <aran.rath@outlook.com>
2026-05-10 14:21:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,46 @@
 from flask import Flask, render_template, request
+import db
 
 app = Flask(__name__)
 
-# Home page
+# initialize database
+db.init_db()
+
+# home page
 @app.route("/")
 def home():
     return render_template("index.html")
 
-# Signup page
+# signup page
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
+
     if request.method == "POST":
-        return "Signup received (not stored yet)"
+
+        username = request.form["username"]
+        password = request.form["password"]
+
+        conn = db.get_db()
+        cursor = conn.cursor()
+
+        try:
+            cursor.execute(
+                "INSERT INTO users (username, password) VALUES (?, ?)",
+                (username, password)
+            )
+
+            conn.commit()
+
+        except:
+            conn.close()
+            return "Username already exists"
+
+        conn.close()
+
+        return "Account created"
+
     return render_template("signup.html")
 
-# Local development only
+# local development
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
