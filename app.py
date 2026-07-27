c3c104b60ba8c007b174001b3d391b00a23cbc3e
CoolCoderAran <aran.rath@outlook.com>
2026-07-27 17:10:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -464,7 +464,7 @@ def login():
         # PASSWORD CHECK
         if not check_password_hash(stored_password, password):
             flash("Incorrect password.")
-            return redirect(url_for("login"))
+            print("Wrong Password")
 
         # SUCCESSFUL LOGIN
         session.permanent = True
