1ec1b0e4ae7046a507c2c677a485c22410401a2f
Aran Rath <aran.rath@outlook.com>
2026-08-22 07:01:56 -0400

    Update planner.js

--- static/planner.js
+++ static/planner.js
@@ -1,1 +1,10 @@
-
+   function toggleSidebar() {
+            const sidebar = document.getElementById("sidebar");
+            const mainContent = document.getElementById("mainContent");
+            
+            // Toggle the 'active' class on the sidebar
+            sidebar.classList.toggle("active");
+            
+            // Optional: Toggle 'shifted' class to push main content
+            mainContent.classList.toggle("shifted");
+        }
